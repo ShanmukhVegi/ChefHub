b9320ae9329767ff4fc84cdf426e889b109ddc3c
HrithikGit <mandhahrithik@gmail.com>
2022-03-07 23:25:43 +0530

    updated SignUp

---
+++ src/screens/auth/signup/SignUp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class SignUp extends React.Component {
+  render() {
+    return (
+      <View >
+        <Text>This is from Signup page</Text>
+      </View>
+    );
+  }
+}
+
+export default SignUp;
+
