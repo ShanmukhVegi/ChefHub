0891c4b4a0824c96c2f0ecabf4579878d2196b53
HrithikGit <mandhahrithik@gmail.com>
2022-03-08 20:48:35 +0530

    added gitignore

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, {Component} from 'react';
 
-import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
--- src/screens/auth/signup/SignUp.js
+++ src/screens/auth/signup/SignUp.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 class SignUp extends React.Component {
   render() {
     return (
-      <View>
-        <Text>You have come to signup.</Text>
-
-        <Button
-          title="Go Home ra pilla bach"
-          onPress={() =>
-            this.props.navigation.navigate('Home')
-          }
-        />
+      <View style = {styles.container}>
+        <Text style={ [styles.fontSize20, styles.marginBottom]} >Sign Up</Text>
+		<View style = { styles.socialLoginContainer }>
+			<View style={ styles.socialLogin }>
+			</View>
+			<View style={ styles.socialLogin }>
+			</View>
+		</View>
+		<View>
+			<Text>Name</Text>
+		</View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+	container : {
+	padding : 30
+	},
+	fontSize20 : {
+		fontSize : 25,
+		fontWeight : "500",
+	},
+	marginBottom : {
+		marginBottom : 30,
+	},
+	socialLoginContainer : {
+		display : 'flex',
+		flexDirection : 'row',
+		justifyContent : 'space-between',
+		marginBottom : 40,
+	},
+	socialLogin : {
+		padding : 30,
+		flex : 0.4,
+		display : 'flex',
+		justifyContent : 'center',
+		alignItems : 'center',
+		backgroundColor : '#e0e0e0',
+		borderRadius : 10,
+		color : '#f1f1f1'
+	},
+	socialImage : {
+		color : 'red',
+	}
+});
 
 export default SignUp;
