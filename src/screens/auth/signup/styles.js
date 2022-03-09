99fb681391cbe877a25c2cf1c7828065e42c85ab
ShanmukhVegi <62086771+ShanmukhVegi@users.noreply.github.com>
2022-03-09 20:58:37 +0530

    Changes in singin and login

---
+++ src/screens/auth/signup/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+module.exports= styles;
